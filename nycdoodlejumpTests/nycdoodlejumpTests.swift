b0afc1052bb640e3cfaea7292f2a616f05ff54de
tjl10-a11y <tjl10@fordham.edu>
2026-01-17 12:14:28 -0500

    Initial Commit

---
+++ nycdoodlejumpTests/nycdoodlejumpTests.swift
@@ -0,0 +1,17 @@
+//
+//  nycdoodlejumpTests.swift
+//  nycdoodlejumpTests
+//
+//  Created by Tenzing Lunn on 1/17/26.
+//
+
+import Testing
+@testable import nycdoodlejump
+
+struct nycdoodlejumpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
